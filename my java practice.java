dcdc2b6b1ecc09f96e4dd97a4cfeea44823e85e9
Mamun Shahriar <mamunshahriar2002@gmail.com>
2025-06-16 22:01:00 +0600

    practice scanner

--- my java practice.java
+++ my java practice.java
@@ -1,3 +1,7 @@
+
+
+
+import java.util.Scanner;
 class FirtClass{
    
     public static void main (String args[]) {
@@ -5,14 +9,49 @@ class FirtClass{
         //variable declaration and initialization
         // String, int, double, char, 
 
-        String name = "Mamun";
-        int age = 25;
-        double salary = 50000.0;
-        char grade = 'A';
+       // String name = "Mamun";
+       // int age = 25;
+       // double salary = 50000.0;
+       // char grade = 'A';
 
-        int a= 10;
-        int b = 20;
+        ///int a= 10;
+       // int b = 5;
+        //int sum = age + b;
+        // Print the variables
 
-       
-    }   
+        //System.out.println(sum);
+
+       // int diff = age - b;
+        //System.out.println(diff);
+
+        //int mul = age * a;
+        // System.out.println(mul);
+
+        //int ans = a * b / a- b;
+       //  System.out.println(ans);
+
+        //int ans2 = (a * b) / (a - b);
+        // System.out.println(ans2);
+
+
+
+        //input
+       // Scanner scanner = new Scanner(System.in);
+
+       //  String inputName = scanner.next();
+       // System.out.print("inputName: " + inputName);
+
+
+       Scanner scanner = new Scanner(System.in);
+
+       int a = scanner.nextInt();
+       int b = scanner.nextInt();
+       int sum2 = a + b;
+       System.out.println("Sum: " + sum2);
+    }
+
+
+
+
+
 }
